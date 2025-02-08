eae95c639f6c7286e614e964880a61a771164bf8
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-08 23:31:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const gridItem   =  document.querySelectorAll("#grid-item");
+const blockInput =  document.querySelector("#block_id");
+const colorInput =  document.querySelector("#colour_id");
+const changeBtn = document.querySelector("#change_button")
+const resetBtn = document.querySelector("#Reset")
+let y = 0
+changeBtn.addEventListener("click",()=>{
+	let x = blockInput.value
+	gridItem[y].style.backgroundColor = "transparent"
+	gridItem.forEach((ele,i)=>{
+		if(x==i+1){
+		  ele.style.backgroundColor = `${colorInput.value}`
+		  y = i
+		}
+	})
+})
+resetBtn.addEventListener("click",()=>{
+	gridItem.forEach((ele,i)=>{
+		ele.style.backgroundColor = "transparent"
+	})
+	// ele.style.backgroundColor = "transparent"
+})
