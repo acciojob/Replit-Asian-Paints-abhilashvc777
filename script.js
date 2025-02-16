246dc3c2c1046ff0363e651ef474e714eaa2bce8
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-16 16:49:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,19 +6,25 @@ const colorInput =  document.querySelector("#colour_id");
 const changeBtn = document.querySelector("#change_button")
 const resetBtn = document.querySelector("#reset_button")
 let y = 0
+let check = false
 changeBtn.addEventListener("click",()=>{
 	let x = blockInput.value
-	gridItem[y].style.opacity = "0.5"
+	if(check){
+	  gridItem[y].style.opacity = "0.5"
+	}
+	// gridItem[y].style.opacity = "1"
 	gridItem.forEach((ele,i)=>{
 		if(x==i+1){
 		  ele.style.backgroundColor = `${colorInput.value}`
 		  y = i
 		}
 	})
+	check = true
 })
 resetBtn.addEventListener("click",()=>{
 	gridItem.forEach((ele,i)=>{
 		ele.style.backgroundColor = ""
 		ele.style.opacity = "1"
 	})
+	check = false;
 })
