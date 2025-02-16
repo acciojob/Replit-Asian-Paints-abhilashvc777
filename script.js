1f2271650c81b28d83fb13c0d99a40f65bacaa58
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2025-02-16 16:29:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const resetBtn = document.querySelector("#reset_button")
 let y = 0
 changeBtn.addEventListener("click",()=>{
 	let x = blockInput.value
-	gridItem[y].style.backgroundColor = "transparent"
+	gridItem[y].style.opacity = "0.5"
 	gridItem.forEach((ele,i)=>{
 		if(x==i+1){
 		  ele.style.backgroundColor = `${colorInput.value}`
